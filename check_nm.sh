6a08a94ddaf1dbfd3cf9b124df67f57e85f3e605
Luca Lombardo <l.lombardo@protonmail.com>
2026-03-20 10:23:16 +0100

    chore: Remove unused scripts for nm symbol checking

--- check_nm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nm local/psblas3/lib/libpsb_cbind.a | grep psb_c_ > nm_output.txt
-cat nm_output.txt
--- run_nm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nm /home/llombardo/hpla-rs/local/psblas3/lib/libpsb_cbind.a > /home/llombardo/hpla-rs/cbind_symbols.txt
-nm /home/llombardo/hpla-rs/local/psblas3/lib/libpsb_base.a > /home/llombardo/hpla-rs/base_symbols.txt
